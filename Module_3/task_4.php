5666e6133dee2446eb4177df5fd0727275db4945
munaimpro <khanmail2599@gmail.com>
2023-09-26 12:48:10 +0600

    array & printing process modified in task_4

--- Module_3/task_4.php
+++ Module_3/task_4.php
@@ -1,19 +1,19 @@
 <?php
     /*-- Declearation & Initialization of studentGrades array --*/
     $studentGrades = [
-        'Student1' => ['Math' => 85, 'English' => 90, 'Science' => 78],
-        'Student2' => ['Math' => 89, 'English' => 70, 'Science' => 77],
-        'Student3' => ['Math' => 88, 'English' => 80, 'Science' => 71]
+        ['Math' => 85, 'English' => 90, 'Science' => 78],
+        ['Math' => 89, 'English' => 70, 'Science' => 77],
+        ['Math' => 88, 'English' => 80, 'Science' => 71]
     ];
 
     /*-- calculateGradeAvg() function --*/
     function calculateGradeAvg(array $studentGrades){
-        foreach($studentGrades as $sutent => $grades):
+        foreach($studentGrades as $student => $grades):
             $sum = array_sum($grades); // Calculating sum for each student
             $totalItem = count($grades); // Calculating total subject for each student
             $average = number_format(($sum / $totalItem), 2); // Calculating average
 
-            echo "Student {$sutent}'s average grade : {$average}\n";
+            echo "Student ".($student + 1)."'s average grade : {$average}\n";
         endforeach;
     }
 
