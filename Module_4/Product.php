ad3423758e5b01c55325ef3c8a43f3b07cf66e5a
munaimpro <khanmail2599@gmail.com>
2023-10-02 22:11:48 +0600

    Folder and Class created

---
+++ Module_4/Product.php
@@ -0,0 +1,19 @@
+php
+class Product {
+    // TODO: Add properties
+    
+    public function __construct($id, $name, $price) {
+        // TODO: Initialize properties
+    }
+
+
+    // TODO: Add getFormattedPrice method
+
+
+    // TODO: Add showDetails method
+}
+
+
+// Test the Product class
+$product = new Product(1, 'T-shirt', 19.99);
+$product->showDetails();
